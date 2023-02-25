6a1c39ccfad1f2cc8ada8450bb96135f6b5d46ce
Влад Сиротюк <vladsyrotiuk@MacBook-Pro-Vlad.local>
2023-02-26 00:32:52 +0200

    refactor(client): ELP-29 fixed test issue

--- client/src/Tenses/Carousel/components/use-practice-data/index.spec.tsx
+++ client/src/Tenses/Carousel/components/use-practice-data/index.spec.tsx
@@ -1,33 +1,42 @@
-// import React from 'react';
-// import { renderHook } from "@testing-library/react"
-// import { get } from 'lodash';
-// import { ResponseContext } from './../../../Main/presentational';
-// import { usePracticeData } from "."
+import React from 'react';
+import { renderHook } from "@testing-library/react"
+import { get } from 'lodash';
+import { usePracticeData } from "."
+import { TenseContext } from '../../../Context';
+
+const mockedApolloResponse = {
+    configuration: {
+      practice: [
+        {
+          sentences: [
+            {
+              id: 1,
+              label: 'Affirmative',
+              partOne: 'Mocked Part One',
+              partTwo: 'Mocked Part Two',
+              missed: 'Mocked Missed Correct Value'
+            }
+          ]
+        }
+      ]
+    },
+  };
 
 
-// describe('usePracticeData', () => {
+describe('usePracticeData', () => {
 
-//     const sentenceMock = [{
-//         id: 1,
-//         label: 'MockLabel',
-//         partOne: 'MockPartOne',
-//         partTwo:'MockPartTwo',
-//         missed: 'MockMissed'
-//     }];
+    const ResponseContextWrapper = ({ children }: { children: JSX.Element }) => (
+        <TenseContext.Provider value={mockedApolloResponse}>{children}</TenseContext.Provider>
+    );
 
-//     const ResponseContextWrapper = ({ children }: { children: JSX.Element }) => (
-//         <ResponseContext.Provider value={sentenceMock}>{children}</ResponseContext.Provider>
-//     );
-
-//     it('should return sentences', () => {
-//         const { result } = renderHook(() => usePracticeData(), { wrapper: ResponseContextWrapper });
-//         const { id, label, partOne, partTwo, missed } = get(result, 'current.sentences[0]')
-//         expect(result.current.sentences.length).toBe(1);
-//         expect(id).toBe(1);
-//         expect(label).toBe('MockLabel');
-//         expect(partOne).toBe('MockPartOne');
-//         expect(partTwo).toBe('MockPartTwo');
-//         expect(missed).toBe('MockMissed');
-//     });
-// })
-export {};
+    it('should return sentences', () => {
+        const { result } = renderHook(() => usePracticeData(), { wrapper: ResponseContextWrapper });
+        const { id, label, partOne, partTwo, missed } = get(result, 'current.sentences[0]')
+        expect(result.current.sentences.length).toBe(1);
+        expect(id).toBe(1);
+        expect(label).toBe('Affirmative');
+        expect(partOne).toBe('Mocked Part One');
+        expect(partTwo).toBe('Mocked Part Two');
+        expect(missed).toBe('Mocked Missed Correct Value');
+    });
+})
