7d0914b8b22d583024c4058ab2de623b83a632a2
Влад Сиротюк <vladsyrotiuk@MacBook-Pro-Vlad.local>
2023-03-02 08:21:53 +0200

    feat(client): ELP-33 implemented mobile menu functionality

--- client/src/Header/header-left-side/index.tsx
+++ client/src/Header/header-left-side/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { SearchOutlined } from '@ant-design/icons';
 import { HeaderInput, HeaderLeft } from '../styled';
+import MobileOptionsMenu from '../../Menu/mobile-menu/options-menu';
 
 const HeaderLeftSide = () => {
   return (
-    <HeaderLeft data-testid="header-left-side">
-      <SearchOutlined />
-      <HeaderInput data-testid="header-input" placeholder='Search for...' />
-    </HeaderLeft>
+    <React.Fragment>
+      <MobileOptionsMenu />
+      <HeaderLeft className='header-left-side' data-testid="header-left-side">
+        <SearchOutlined />
+        <HeaderInput data-testid="header-input" placeholder='Search for...' />
+      </HeaderLeft>
+    </React.Fragment>
   );
 }
 
