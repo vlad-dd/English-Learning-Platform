6f3e4e4cb729ccd46aef564d49c2cecd1d532669
Влад Сиротюк <vladsyrotiuk@MacBook-Pro-Vlad.local>
2023-03-24 00:39:43 +0200

    feat(client): ELP-53 added reducer from send report part

--- client/src/store/reducers/root.tsx
+++ client/src/store/reducers/root.tsx
@@ -3,12 +3,14 @@ import { reducer as breadCrumb } from './bread-crumb';
 import { reducer as registerSlice } from './registration';
 import { reducer as carouselSlice } from './carousel';
 import { reducer as tenseSlice } from './tenses';
+import { reducer as SendReportSlice } from './send-report';
 
 const root = combineReducers({
     tensesBreadCrumb: breadCrumb,
     registration: registerSlice,
     carousel: carouselSlice,
-    tense: tenseSlice
+    tense: tenseSlice,
+    sendReport: SendReportSlice
 });
 
 export default root;
