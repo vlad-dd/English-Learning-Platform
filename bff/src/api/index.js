4ddaba1caf8f7168c6222f3f6da555724d3086b8
Влад Сиротюк <vladsyrotiuk@MacBook-Pro-Vlad.local>
2023-04-17 02:51:52 +0300

    feat(server): ELP-75 added support for saving comment reports

--- bff/src/api/index.js
+++ bff/src/api/index.js
@@ -1,12 +1,48 @@
+const { collection, getDocs, doc, getDoc, updateDoc } = require('firebase/firestore');
 const { default: axios } = require('axios');
+const { database } = require("../../bff-base");
+const { uid, generateDate } = require('../utils/utils');
 
-const BFF_API = {
+const mergeDataWithDocument = async (collection, document, newData, container) => {
+    const docRef = doc(database, collection, document);
+    const docSnap = await getDoc(docRef);
+    const existingData = docSnap.exists() ? docSnap.data() : {};
+    const mergedData = {
+        [container]: [...(existingData[container] || []), newData]
+      };
+    await updateDoc(docRef, mergedData);
+}
+
+const BFF_API_QUERIES = {
+    getVerbConfiguration(tense) {
+        return getDocs(collection(database, tense));
+    },
+    getReleasesTimelineConfiguration() {
+        return getDocs(collection(database, 'Releases'));
+    },
+    getGrammarByLevelConfiguration(level, theme) {
+        return getDoc(doc(database, level, theme));
+    },
+    getTestsConfiguration(part, theme) {
+        return getDoc(doc(database, part, theme));
+    },
     async searchForWordInDictionary(word) {
         const { data } = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
         return data;
+    },
+};
+
+const BFF_MUTATIONS = {
+    async addComment(collection, id, comment) {
+        await mergeDataWithDocument(collection, id, { id: uid(9), date: generateDate() , comment }, "comments");
+    },
+    async reportComment(report) {
+        console.log('report: ', report)
+        await mergeDataWithDocument("Support", "Report-Comment", { id: uid(9), report }, "reports");
     }
-}
+};
 
 module.exports = {
-    BFF_API
+    BFF_API_QUERIES,
+    BFF_MUTATIONS
 }
--- bff/src/resolvers/Query.js
+++ bff/src/resolvers/Query.js
@@ -1,51 +1,39 @@
-const { collection, getDocs, doc, getDoc, updateDoc } = require('firebase/firestore');
 const { get } = require('lodash');
-const { database } = require("../../bff-base");
-const { BFF_API } = require("../api/index");
-const { 
-  uid,
-  generateDate
-} = require("../utils/utils");
+const { BFF_API_QUERIES, BFF_MUTATIONS } = require("../api/index");
 
 const resolvers = {
   Query: {
     countOfTenses: async(root, { tense }) => {
-      const data = await getDocs(collection(database, tense));
+      const data = await BFF_API_QUERIES.getVerbConfiguration(tense);
       return (data.docs.map((doc) => ({...doc.data()})))
     },
     dictionary: (root, { word }) => {
-      return BFF_API.searchForWordInDictionary(word);
+      return BFF_API_QUERIES.searchForWordInDictionary(word);
     },
     releases: async() => {
-      const data = await getDocs(collection(database, 'Releases'))
+      const data = await BFF_API_QUERIES.getReleasesTimelineConfiguration();
       const logged = data.docs.map((doc) => ({...doc.data()}));
       const releaseConfiguration = get(logged, '[0].releases').reverse();
       return releaseConfiguration;
     },
     grammarByLevel: async(root, { level, theme }) => {
-      const grammarConfiguration = doc(database, level, theme);
-      const grammarDocument = await getDoc(grammarConfiguration)
+      const grammarDocument = await BFF_API_QUERIES.getGrammarByLevelConfiguration(level, theme);
       return [grammarDocument.data()];
     },
     getTests: async(root, { part, theme }) => {
-      const testConfiguration = doc(database, part, theme)
-      const testDocument = await getDoc(testConfiguration)
+      const testDocument = await BFF_API_QUERIES.getTestsConfiguration(part, theme);
       return [testDocument.data()];
     }
   },
   Mutation: {
     addComment: async (root, { collection = 'Comments', id = 'Comments', comment }) => {
-      const docRef = doc(database, collection, id);
-      const docSnap = await getDoc(docRef);
-      const existingData = docSnap.exists() ? docSnap.data() : {};
-      const newData = {
-        comments: [...(existingData.comments || []), { id: uid(9), date: generateDate() , comment }]
-      };
-      await updateDoc(docRef, newData);
+      await BFF_MUTATIONS.addComment(collection, id, comment);
+    },
+    reportComment: async (root, { report }) => {
+      await BFF_MUTATIONS.reportComment(report);
     }
-  }
-  
-}
+  } 
+};
 
 module.exports = {
     resolvers
