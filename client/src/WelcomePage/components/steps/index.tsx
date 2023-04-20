167b6e8135a8ec8c8fac98bbe57266786fbf0b1f
Влад Сиротюк <vladsyrotiuk@MacBook-Pro-Vlad.local>
2023-04-20 13:54:13 +0300

    fix(ELP-77): ELP-77 steps should run automatically once component mount

--- client/src/WelcomePage/components/steps/index.tsx
+++ client/src/WelcomePage/components/steps/index.tsx
@@ -1,24 +1,32 @@
-import React, { useState } from "react";
+import React, { useLayoutEffect, useState } from "react";
+import { size } from "lodash";
 import { GUIDELINE_STEPS } from "../../constants";
 import { StyledSteps, StyledTitle } from "./styled";
 
 const StepsGuideline = () => {
-    const [current, setCurrent] = useState(0);
-    const onChange = (value: number) => {
-        setCurrent(value);
-      };
-    return (
-        <React.Fragment>
-            <StyledTitle>How to use English Learning Platform? 🤔</StyledTitle>
-            <StyledSteps
-              data-testid="steps-guideline"
-              current={current}
-              onChange={onChange}
-              direction="horizontal"
-              items={GUIDELINE_STEPS}
-            />
-        </React.Fragment>
-    )
+  const [current, setCurrent] = useState(0);
+
+  useLayoutEffect(() => {
+    const elementToClick = document.querySelectorAll('div[role="button"]')[1];
+    elementToClick?.dispatchEvent(new Event('click', { bubbles: true }));
+  }, []);
+
+  return (
+    <React.Fragment>
+      <StyledTitle>How to use English Learning Platform? 🤔</StyledTitle>
+      <StyledSteps
+        data-testid="steps-guideline"
+        current={current}
+        onChange={() => {
+          if (current < size(GUIDELINE_STEPS)) {
+            setInterval(() => setCurrent((prev: number) => prev + 1), 3500)
+          }
+        }}
+        direction="horizontal"
+        items={GUIDELINE_STEPS}
+      />
+    </React.Fragment>
+  )
 }
 
 export default StepsGuideline;
