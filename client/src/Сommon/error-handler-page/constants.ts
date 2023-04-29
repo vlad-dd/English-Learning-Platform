9b6e7606bb170ceede826c84b6870c5b7ba86d64
Влад Сиротюк <vladsyrotiuk@MacBook-Pro-Vlad.local>
2023-04-30 00:33:31 +0300

    feat(client): ELP-85 error page was added

---
+++ client/src/Сommon/error-handler-page/constants.ts
@@ -0,0 +1,6 @@
+export const ELP_USER_EXPERIENCE_ERRORS = {
+    BAD_URL: `Oops! It looks like you tried to access an uncharted territory in space. Unfortunately, the planet you are looking for does not exist or may have been destroyed. Please check your coordinates and try again.`,
+    BAD_CONNECTION: `Houston we have a problem. It seems we're experiencing a communication error with our home planet. Our space engineers are working hard to fix the issue, but until then, we're unable to display the requested content. Please check your network connection and try again. In the meantime, feel free to gaze out into the vast expanse of space and contemplate the mysteries of the universe.`,
+    REQUEST_ERROR: `We've encountered a problem while trying to retrieve data from the server. It seems our communication with Earth has been disrupted. Our team of highly-trained astronauts is working hard to fix the issue and restore communication as soon as possible.`,
+    UNEXPECTED_BREAK: `It looks like something went wrong in space! Don't worry though, our team of intrepid space explorers are on the case and will have things back up and running in no time.`
+};
---
+++ client/src/Сommon/error-handler-page/index.ts
@@ -0,0 +1,1 @@
+export { default as NotFoundURL } from './not-found-url';
