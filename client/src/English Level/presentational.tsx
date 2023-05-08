d988830b08d06497b615e150bb0137aec98ffcb9
Влад Сиротюк <vladsyrotiuk@MacBook-Pro-Vlad.local>
2023-05-08 12:45:13 +0300

    feat(client): ELP-93 added test bar

--- client/src/English Level/presentational.tsx
+++ client/src/English Level/presentational.tsx
@@ -1,16 +1,17 @@
 import Quiz from 'react-quiz-component';
 import { StyledTitle } from "./styled";
-import CheckIcon from '@mui/icons-material/Check';
-import CloseIcon from '@mui/icons-material/Close';
-import { size } from 'lodash';
+import { noop, size } from 'lodash';
+import { Steps } from 'antd';
 import { useState } from 'react';
-import { FcOk } from 'react-icons/fc'
-import CloudDoneIcon from '@mui/icons-material/CloudDone';
 
 export const quiz = {
-    "quizTitle": "React Quiz Component Demo",
-    "quizSynopsis": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim",
-    "nrOfQuestions": "4",
+    "quizTitle": <h1>Greetings and welcome to the English proficiency assessment! 🌍</h1>,
+    "quizSynopsis":
+        <div>
+            <h3>✅ Kindly attempt all questions.</h3>
+            <h3>👀 The assessment does not have time limits.</h3>
+            <h3>🤷🏽 In case you're unsure of the answer, please select the answer you think could be true.</h3>
+        </div>,
     "questions": [
         {
             "question": "How can you access the state of a component from inside of a member function?",
@@ -30,131 +31,120 @@ export const quiz = {
             "point": "20"
         },
         {
-            "question": "ReactJS is developed by _____?",
+            "question": "How can you access the state of a component from inside of a member function?",
             "questionType": "text",
+            "questionPic": "https://dummyimage.com/600x400/000/fff&text=X", // if you need to display Picture in Question
             "answerSelectionType": "single",
             "answers": [
-                "Google Engineers",
-                "Facebook Engineers"
-            ],
-            "correctAnswer": "2",
-            "messageForCorrectAnswer": "Correct answer. Good job.",
-            "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
-            "explanation": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-            "point": "20"
-        },
-        {
-            "question": "ReactJS is an MVC based framework?",
-            "questionType": "text",
-            "answerSelectionType": "single",
-            "answers": [
-                "True",
-                "False"
-            ],
-            "correctAnswer": "2",
-            "messageForCorrectAnswer": "Correct answer. Good job.",
-            "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
-            "explanation": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-            "point": "10"
-        },
-        {
-            "question": "Which of the following concepts is/are key to ReactJS?",
-            "questionType": "text",
-            "answerSelectionType": "single",
-            "answers": [
-                "Component-oriented design",
-                "Event delegation model",
-                "Both of the above",
+                "this.getState()",
+                "this.prototype.stateValue",
+                "this.state",
+                "this.values"
             ],
             "correctAnswer": "3",
             "messageForCorrectAnswer": "Correct answer. Good job.",
             "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
             "explanation": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-            "point": "30"
+            "point": "20"
         },
         {
-            "question": "Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
-            "questionType": "photo",
+            "question": "How can you access the state of a component from inside of a member function?",
+            "questionType": "text",
+            "questionPic": "https://dummyimage.com/600x400/000/fff&text=X", // if you need to display Picture in Question
             "answerSelectionType": "single",
             "answers": [
-                "https://dummyimage.com/600x400/000/fff&text=A",
-                "https://dummyimage.com/600x400/000/fff&text=B",
-                "https://dummyimage.com/600x400/000/fff&text=C",
-                "https://dummyimage.com/600x400/000/fff&text=D"
+                "this.getState()",
+                "this.prototype.stateValue",
+                "this.state",
+                "this.values"
             ],
-            "correctAnswer": "1",
+            "correctAnswer": "3",
             "messageForCorrectAnswer": "Correct answer. Good job.",
             "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
             "explanation": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
             "point": "20"
         },
         {
-            "question": "What are the advantages of React JS?",
+            "question": "How can you access the state of a component from inside of a member function?",
             "questionType": "text",
-            "answerSelectionType": "multiple",
+            "questionPic": "https://dummyimage.com/600x400/000/fff&text=X", // if you need to display Picture in Question
+            "answerSelectionType": "single",
             "answers": [
-                "React can be used on client and as well as server side too",
-                "Using React increases readability and makes maintainability easier. Component, Data patterns improves readability and thus makes it easier for manitaining larger apps",
-                "React components have lifecycle events that fall into State/Property Updates",
-                "React can be used with any other framework (Backbone.js, Angular.js) as it is only a view layer"
+                "this.getState()",
+                "this.prototype.stateValue",
+                "this.state",
+                "this.values"
             ],
-            "correctAnswer": [1, 2, 4],
+            "correctAnswer": "3",
             "messageForCorrectAnswer": "Correct answer. Good job.",
             "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
             "explanation": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
             "point": "20"
         },
+        {
+            "question": "How can you access the state of a component from inside of a member function?",
+            "questionType": "text",
+            "questionPic": "https://dummyimage.com/600x400/000/fff&text=X", // if you need to display Picture in Question
+            "answerSelectionType": "single",
+            "answers": [
+                "this.getState()",
+                "this.prototype.stateValue",
+                "this.state",
+                "this.values"
+            ],
+            "correctAnswer": "3",
+            "messageForCorrectAnswer": "Correct answer. Good job.",
+            "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
+            "explanation": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+            "point": "20"
+        }
     ]
 }
 
 const EnglishLevelRoot = () => {
     const [counter, setCounter] = useState(0);
-    const [indexes, setIndex] = useState([{}]);
-    const [total, setTotal] = useState([{}])
-    const [view, setView] = useState<any>()
+    const [total, setTotal] = useState<any>([{}])
 
     const checkIsAnswered = (ew: any) => {
         const { userAnswer, question: { correctAnswer } } = ew;
-        setIndex((prev: any) => ({
-            ...prev,
-            [counter]: 1
-        }))
-        setTotal((prev: any) => ({
-            ...prev,
-            [counter]: userAnswer === parseInt(correctAnswer)
-        }))
+        setTotal((prev: any) => {
+            const updatedTotal = {
+                title: JSON.stringify(counter),
+                [counter]: userAnswer === parseInt(correctAnswer)
+            };
+            if (counter > 0) {
+                return [...prev, updatedTotal];
+            } else {
+                return [updatedTotal];
+            }
+        });
         setCounter((prev: number) => prev + 1)
     }
 
-    const setQuizResult = (obj: any) => {
-        console.log(obj);
-        setView(total)
-    }
+    console.log('counter', counter)
+    console.log('total', total)
+    console.log('total[counter]', total[counter - 1])
 
-    console.log('view: ', view)
-
-
-    console.log('INDEXES: ', indexes)
-    console.log('total: ', total)
+    //to make steps adaptive add to the second div flexWrap: wrap
 
     return (
         <div>
             <StyledTitle className="elp-title">English Proficiency Level Test</StyledTitle>
-            <div style={{ display: "flex" }}>
-                <Quiz quiz={quiz} onQuestionSubmit={checkIsAnswered} onComplete={setQuizResult} />
-                <div style={{ display: "flex", height: "100%" }}>
-                    {new Array(size(quiz.questions)).fill(0).map((el: any, index: number) => {
-                        return !(!!size(view)) ? (
-                            <div style={{ border: "1px solid white", marginLeft: "1vw" }}>
-                                {indexes[index] === 1 ? <div style={{ padding: "15px" }}><CloudDoneIcon /></div> : <div style={{ padding: "15px" }}>{index}</div>}
-                                {/* <div  style={{ background: "green", padding: "5px" }}><CheckIcon /></div> */}
-                            </div>
-                        ) : <div style={{ border: "1px solid white", marginLeft: "1vw" }}>
-                            {view[index] === true ? <div style={{ padding: "15px", display: "flex", flexDirection: "column"}}><FcOk size={25} /></div> : <div style={{ padding: "15px" }}><CloseIcon /></div>}
-                            {/* <div  style={{ background: "green", padding: "5px" }}><CheckIcon /></div> */}
-                        </div>
-                    })}
+            <div style={{ display: "flex", alignItems: "flex-start", overflow: "auto" }}>
+                <div>
+                    <Quiz quiz={quiz} onQuestionSubmit={checkIsAnswered} />
                 </div>
+                <Steps
+                    current={counter}
+                    style={{ display: "flex", flexDirection: "row", flexWrap: "wrap", position: "relative", top: "3vh" }}
+                    direction='vertical'
+                    responsive
+                >
+                    {new Array(size(quiz.questions)).fill(0).map((_, i) => {
+                        //@ts-ignore
+                        return <Steps.Step status={total[i]?.[i] === false && "error"}>{i}</Steps.Step>
+                    })}
+                </Steps>
             </div>
         </div>
     )
