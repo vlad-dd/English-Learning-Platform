ecf63344d30fc2945051e5a72a625b8fcb87db35
Влад Сиротюк <vladsyrotiuk@MacBook-Pro-Vlad.local>
2023-05-22 09:56:38 +0300

    refactor(client): ELP-105 build apollo client method was added

---
+++ client/src/test-utils/index.tsx
@@ -0,0 +1,8 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+export const buildApolloClientInstance = () => {
+    return new ApolloClient({
+        uri: "http://localhost:4000",
+        cache: new InMemoryCache(),
+    });
+}
