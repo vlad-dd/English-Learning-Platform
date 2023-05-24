23df32d6d456a772572d51f1e483ec07e76e5b02
Влад Сиротюк <vladsyrotiuk@MacBook-Pro-Vlad.local>
2023-05-24 10:49:29 +0300

    feat(client): ELP-107 added some hoc providers for tests

--- client/src/test-utils/hocs/index.tsx
+++ client/src/test-utils/hocs/index.tsx
@@ -1,7 +1,14 @@
 import { ApolloProvider } from "@apollo/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "../../store";
 import { buildApolloClientInstance } from "..";
 
-export const withApolloProvider = (Component: any) => {
+export const withApolloProvider = (Component: Function) => {
     const apolloClientInstance = buildApolloClientInstance();
-    return (props: any) => <ApolloProvider client={apolloClientInstance} children={<Component {...props} />} />
+    return (props: Record<string, any>) => <ApolloProvider client={apolloClientInstance} children={<Component {...props} />} />
 };
+
+export const withRouterProvider = (Component: Function) => (props: Record<string, any>) => <BrowserRouter children={<Component {...props} />} />
+
+export const withReduxProvider = (Component: Function) => (props: Record<string, any>) => <Provider store={store} children={<Component {...props} />} />
