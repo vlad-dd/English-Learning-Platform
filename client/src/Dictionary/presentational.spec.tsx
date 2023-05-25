d37ea572594042fc2a08c7e30d8f8bf580280e2f
Влад Сиротюк <vladsyrotiuk@MacBook-Pro-Vlad.local>
2023-05-25 17:57:13 +0300

    feat(client): ELP-108 added test for dictionary app

--- client/src/Dictionary/presentational.spec.tsx
+++ client/src/Dictionary/presentational.spec.tsx
@@ -1,8 +1,10 @@
-import React from "react";
 import { render, screen } from "@testing-library/react";
 import Dictionary from "./presentational";
 import { DictionaryConfigurationContext } from "./Context";
-import { DICTIONARY_TIP } from "./constants";
+import { DICTIONARY_TIP, UNEXISTED_WORD_DESCRIPTION } from "./constants";
+import { BrowserRouter } from "react-router-dom";
+import { APOLLO_GRAPHQL_ERRORS, ELP_USER_EXPERIENCE_ERRORS } from "../Сommon/error-handler-page/constants";
+import { buildApolloError } from "../test-utils";
 
 const responseMock = {
     dictionary: [
@@ -63,19 +65,19 @@ describe('Dictionary', () => {
             expect(screen.getByTestId('content-section-wrapper')).toBeInTheDocument();
             expect(screen.getByTestId('dictionary-wrapper')).toBeInTheDocument();
         });
-    
+
         it('should render tip alert', () => {
             expect(screen.getByText(DICTIONARY_TIP)).toBeInTheDocument();
             expect(screen.getByText(DICTIONARY_TIP).getAttribute('class')).toBe('ant-alert-message');
         });
-    
+
         it('should render search input', () => {
             const searchInputId = 'dictionary-search-input';
             expect(screen.getByTestId(searchInputId)).toBeInTheDocument();
             expect(screen.getByText('Search')).toBeInTheDocument();
             expect(screen.getByTestId(searchInputId).getAttribute("placeholder")).toBe('Search word...');
         });
-    
+
         it('should render word definitions table', () => {
             const defitionsTableId = 'definitions-table';
             const expectedValues = ['Part Of Speech', 'Definitions', 'Mocked Part Of Speech', 'Mocked Definition'];
@@ -83,7 +85,7 @@ describe('Dictionary', () => {
             expect(screen.getByTestId(defitionsTableId).getAttribute('class')).toBe('ant-table');
             expectedValues.forEach((value: string) => expect(screen.getByText(value)).toBeInTheDocument());
         });
-    })
+    });
 
     describe('while isLoading true', () => {
         it('should render Dictionary root', () => {
@@ -91,9 +93,37 @@ describe('Dictionary', () => {
                 <DictionaryConfigurationContext.Provider value={{ data: responseMock, isLoading: true, searchWordInDictionary: jest.fn(), error: undefined }}>
                     <Dictionary />
                 </DictionaryConfigurationContext.Provider>
-            )
-            expect(screen.getByTestId('dictionary-wrapper')).toBeInTheDocument();
+            );
             expect(screen.getByTestId('loading-progress')).toBeInTheDocument();
         });
     });
-})
+
+    describe('when user entered unexisted word', () => {
+        it('should render Dictionary root', () => {
+            render(
+                <DictionaryConfigurationContext.Provider value={{ data: responseMock, isLoading: true, searchWordInDictionary: jest.fn(), error: buildApolloError(APOLLO_GRAPHQL_ERRORS.REQUEST_FAILED_404) }}>
+                    <Dictionary />
+                </DictionaryConfigurationContext.Provider>
+            );
+            screen.debug();
+            expect(screen.getByTestId("error-empty-space")).toBeInTheDocument();
+            expect(screen.getByText(UNEXISTED_WORD_DESCRIPTION)).toBeInTheDocument();
+        });
+    });
+
+    describe('when user has a bad connection', () => {
+        it('should show that connection is bad', () => {
+            const windowNavigatorSpy = jest.spyOn(window.navigator, 'onLine', 'get');
+            windowNavigatorSpy.mockReturnValue(false);
+            render(
+                <BrowserRouter>
+                    <DictionaryConfigurationContext.Provider value={{ data: responseMock, isLoading: false, searchWordInDictionary: jest.fn(), error: undefined }}>
+                        <Dictionary />
+                    </DictionaryConfigurationContext.Provider>
+                </BrowserRouter>
+            );
+            expect(screen.getByTestId("error-page")).toBeInTheDocument();
+            expect(screen.getByText(ELP_USER_EXPERIENCE_ERRORS.BAD_CONNECTION)).toBeInTheDocument(); 
+        });
+    });
+});
