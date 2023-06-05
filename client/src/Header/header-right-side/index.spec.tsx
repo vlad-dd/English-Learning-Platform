b0ffd6113e234e1c1913528f3273ebf634439169
Влад Сиротюк <vladsyrotiuk@MacBook-Pro-Vlad.local>
2023-06-06 02:12:23 +0300

    refactor(client): ELP-116 got rid of tense providers

--- client/src/Header/header-right-side/index.spec.tsx
+++ client/src/Header/header-right-side/index.spec.tsx
@@ -1,62 +1,46 @@
-//@ts-nocheck
 import { fireEvent, render, screen } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
 import { noop } from 'lodash';
-import ErrorBoundary from '../../ErrorBoundary';
-import store from '../../store';
-import { ThemeContext } from '../../Contexts';
 import HeaderRightSide from '.';
+import { withReduxProvider, withRouterProvider } from '../../test-utils/hocs';
 
-const ApplicationProviders = ({ children }: { children: JSX.Element }) => {
-    return (
-        <ErrorBoundary>
-            <BrowserRouter>
-                <Provider store={store}>
-                    {children}
-                </Provider>
-            </BrowserRouter>
-        </ErrorBoundary>
-    );
-};
+const HeaderRightSideWithProvider = withRouterProvider(withReduxProvider(HeaderRightSide));
 
 describe('HeaderLeftSide', () => {
 
     describe('When user exists', () => {
         const handleSignOut = jest.fn();
+
         beforeEach(() => {
-            render(<ApplicationProviders><HeaderRightSide enteredUser={{ email: 'test@gmail.com' }} handleSignOut={handleSignOut} /></ApplicationProviders>)
-        })
+            render(<HeaderRightSideWithProvider enteredUser={{ email: 'test@gmail.com' }} handleSignOut={handleSignOut} />);
+        });
 
         it('should render HeaderLeftSide', () => {
             expect(screen.getByText('test@gmail.com')).toBeInTheDocument();
             expect(screen.queryAllByRole('img')[0]).toBeInTheDocument();
-        })
+        });
 
         it('should render header dropdown menu', () => {
             const menuItems = ['Profile', 'Sign out'];
             fireEvent.click(screen.getByText('test@gmail.com'))
             expect(screen.getByTestId("header-dropdown-menu")).toBeInTheDocument();
             menuItems.forEach((menuItem) => expect(screen.getByText(menuItem)).toBeInTheDocument())
-        })
+        });
 
         it('should call handleSignOut method if clicked', () => {
             fireEvent.click(screen.getByText('test@gmail.com'))
             fireEvent.click(screen.getByText('Sign out'))
             expect(handleSignOut).toHaveBeenCalled();
-        })
-
-    })
+        });
+    });
 
     describe('When user does not exist', () => {
         beforeEach(() => {
-            render(<ApplicationProviders><HeaderRightSide enteredUser={null} handleSignOut={noop} /></ApplicationProviders>)
-        })
+            render(<HeaderRightSideWithProvider enteredUser={null} handleSignOut={noop} />);
+        });
 
         it('should render HeaderLeftSide', () => {
             expect(screen.getByText('Guest')).toBeInTheDocument();
             expect(screen.queryAllByRole('img')[0]).toBeInTheDocument();
-        })
-    })
-
-})
+        });
+    });
+});
