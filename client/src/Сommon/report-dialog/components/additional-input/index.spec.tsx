9810973f1c890c20a5a44e3f01de81917588ef0f
Влад Сиротюк <vladsyrotiuk@MacBook-Pro-Vlad.local>
2023-06-24 17:50:16 +0300

    refactor(client): ELP-134 tests were fixed due to the latest cleanup

--- client/src/Сommon/report-dialog/components/additional-input/index.spec.tsx
+++ client/src/Сommon/report-dialog/components/additional-input/index.spec.tsx
@@ -1,8 +1,12 @@
 import { fireEvent, render, screen } from '@testing-library/react'
 import * as Hooks from '../../../../Hooks';
+import { withIntlProvider } from '../../../../test-utils/hocs';
 import AdditionalInput from '.';
+import { ADDITIONAL_REPORT_MODAL_INPUT_DATA_TEST_ID } from '../../constants';
 
 jest.mock("../../../../Hooks");
+
+const AdditionalInputWithProvider = withIntlProvider(AdditionalInput);
 
 describe('AdditionalInput', () => {
     const label = "Want to add an additional information?";
@@ -10,22 +14,22 @@ describe('AdditionalInput', () => {
     const handleChange = jest.fn();
     it('should not render the additional input', () => {
         useBooleanSpy.mockReturnValue([false, jest.fn()]);
-        render(<AdditionalInput setAdditionalInformation={handleChange} />)
+        render(<AdditionalInputWithProvider setAdditionalInformation={handleChange} />)
         expect(screen.getByText(label)).toBeInTheDocument();
-        expect(screen.queryByTestId("additional-report-modal-input")).not.toBeInTheDocument();
+        expect(screen.queryByTestId(ADDITIONAL_REPORT_MODAL_INPUT_DATA_TEST_ID)).not.toBeInTheDocument();
     });
 
     it('should render an additional input if useBoolean returns true flag', () => {
         useBooleanSpy.mockReturnValue([true, jest.fn()]);
-        render(<AdditionalInput setAdditionalInformation={handleChange} />)
+        render(<AdditionalInputWithProvider setAdditionalInformation={handleChange} />)
         expect(screen.getByText(label)).toBeInTheDocument();
-        expect(screen.queryByTestId("additional-report-modal-input")).toBeInTheDocument();
+        expect(screen.queryByTestId(ADDITIONAL_REPORT_MODAL_INPUT_DATA_TEST_ID)).toBeInTheDocument();
     });
 
     it('should call handleChange if user makes change', () => {
         useBooleanSpy.mockReturnValue([true, jest.fn()]);
-        render(<AdditionalInput setAdditionalInformation={handleChange} />)
-        fireEvent.change(screen.getByTestId("additional-report-modal-input"), { target: { value: 'user-input' } })
+        render(<AdditionalInputWithProvider setAdditionalInformation={handleChange} />);
+        fireEvent.change(screen.getByTestId(ADDITIONAL_REPORT_MODAL_INPUT_DATA_TEST_ID), { target: { value: 'user-input' } })
         expect(handleChange).toHaveBeenCalledTimes(1);
         expect(handleChange).toHaveBeenCalledWith("user-input");
     });
