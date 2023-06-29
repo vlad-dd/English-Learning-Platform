afe1fb9ace33e348392de6e61eb08b358d725dd0
Влад Сиротюк <vladsyrotiuk@MacBook-Pro-Vlad.local>
2023-06-29 17:35:47 +0300

    refactor(client): ELP-138 application menu cleanup

--- client/src/Menu/mobile-menu/index.tsx
+++ client/src/Menu/mobile-menu/index.tsx
@@ -1,26 +1,27 @@
-import { Menu } from 'antd';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router';
-import { options } from '../options';
 import { breadcrumbPath } from '../../store/reducers/bread-crumb';
 import { setActualSentenceIndex, setEarnedPoints } from '../../store/reducers/carousel';
+import { options } from '../options';
+import { IKeyPath, IMobileSiderMenu } from '../types';
+import { BURGER_MENU_OPTIONS_DATA_TEST_ID } from '../constants';
+import { StyledMobileSiderMenu } from '../styled';
 
-const MobileSiderMenu = ({ onClose }: { onClose: () => void }) => {
+const MobileSiderMenu = ({ onClose }: IMobileSiderMenu) => {
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
     return (
-        <Menu
-            style={{ height: "100vh", width: "100vw" }}
-            data-testid="burger-menu"
+        <StyledMobileSiderMenu
+            data-testid={BURGER_MENU_OPTIONS_DATA_TEST_ID}
             mode="inline"
             items={options}
-            onSelect={({ keyPath }: { keyPath: string[] }) => {
+            onSelect={({ keyPath }: IKeyPath) => {
                 navigate(`/${keyPath.reverse().join('/')}`)
                 dispatch(breadcrumbPath(keyPath.reverse()));
                 dispatch(setActualSentenceIndex(0));
                 dispatch(setEarnedPoints(0));
-                onClose();
+                setTimeout(() => onClose())
             }}
         />
     );
--- client/src/Menu/presentational.tsx
+++ client/src/Menu/presentational.tsx
@@ -1,9 +1,11 @@
-import { Menu } from 'antd';
-import { useNavigate } from 'react-router';
-import { options } from './options';
-import { breadcrumbPath } from '../store/reducers/bread-crumb';
 import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router';
+import { Menu } from 'antd';
 import { setActualSentenceIndex, setEarnedPoints } from '../store/reducers/carousel';
+import { breadcrumbPath } from '../store/reducers/bread-crumb';
+import { options } from './options';
+import { IKeyPath } from './types';
+import { MENU_OPTIONS_DATA_TEST_ID } from './constants';
 
 const SiderMenu = () => {
   const dispatch = useDispatch();
@@ -11,13 +13,13 @@ const SiderMenu = () => {
   
   return (
       <Menu
-        data-testid="options-menu"
+        data-testid={MENU_OPTIONS_DATA_TEST_ID}
         defaultSelectedKeys={['1']}
         mode="inline"
         theme="dark"
         items={options}
-        onSelect={({ keyPath }: { keyPath: string[] }) => {
-          navigate(`/${keyPath.reverse().join('/')}`)
+        onSelect={({ keyPath }: IKeyPath) => {
+          navigate(`/${keyPath.reverse().join('/')}`);
           dispatch(breadcrumbPath(keyPath.reverse()));
           dispatch(setActualSentenceIndex(0));
           dispatch(setEarnedPoints(0));
